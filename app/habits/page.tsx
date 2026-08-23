4406f30293d30d5fe0ddd24b9e5998cc74086613
Neerrr001 <neeragarwal711@gmail.com>
2026-08-23 19:34:20 +0530

    added prisma orm and created a postgresSQL database

--- app/habits/page.tsx
+++ app/habits/page.tsx
@@ -1,10 +1,21 @@
 import HabitList from "@/app/habits/HabitList"
+import { useState } from 'react';
+import {Habit} from "@/app/types"
+
+
+
+
+const [habits, setHabits] = useState<Habit[]>([    
+        {id: 1, name: "workout", completed: false},
+        {id: 2, name: "gym" , completed: false},
+        {id: 3, name: "read", completed: false}
+    ])
 
 
 function page() {
   return (
     <div className="text-xl font-medium p-4">
-        <HabitList />
+        <HabitList habits={habits} setHabits={setHabits}/>
       
     </div>
   )
