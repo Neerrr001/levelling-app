2ce195015ce6ca4cb7b41cbbd2d1438f40f69881
Neerrr001 <neeragarwal711@gmail.com>
2026-08-26 15:12:52 +0530

    Made PrismaClient available to the entire app

---
+++ lib/prisma.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "../app/generated/prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
+}) // creates a postgresql connection adapter for prisma 
+
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient;
+} // to put our prisma client somewhere global so we can reuse the same instance
+
+const prisma = globalForPrisma.prisma || new PrismaClient({
+    adapter,
+})
+
+
+if(process.env.NODE_ENV !== "production"){
+    globalForPrisma.prisma = prisma; 
+}
+
+
+export default prisma 
