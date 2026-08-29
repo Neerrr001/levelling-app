d9e23cd080a8a56026a35e377394ee3664978fb6
Neerrr001 <neeragarwal711@gmail.com>
2026-08-29 12:54:54 +0530

    Added a habit using form

--- app/habits/HabitList.tsx
+++ app/habits/HabitList.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useState } from 'react'
 import { Habit } from "@/app/types"
+import { addHabit } from "@/lib/actions"
 
 type HabitListProps = {
     habits:Habit[];
@@ -8,69 +9,19 @@ type HabitListProps = {
 
 function HabitList({habits}: HabitListProps ) {
     
-    // const [inputHabitName, setInputHabitName] = useState<string>("")
-
-    // function handleHabitCompletion(id:number){
-    //     const newHabits = habits.map((habit)=>{
-    //         if(habit.id === id){
-    //             return {
-    //                 ...habit,
-    //                 completed: !habit.completed
-    //             }
-    //         }else{
-    //             return habit
-    //         }
-    //     })
-    //     setHabits(newHabits)
-    // }
-    
-    // function handleButtonClick(){
-    //     const newHabit = {
-    //         id:habits.length + 1,
-    //         name: inputHabitName,
-    //         completed:false
-    //     }
-    //     setHabits([...habits,newHabit])
-    //     setInputHabitName("");
-    // }
-
   return (
     <div className=''>
-        {/* <div className='flex gap-2'>
-            <div>
-                <input type="text"
-                className='border rounded-lg focus:ring p-2'
-                value={inputHabitName}
-                onChange={(e)=> setInputHabitName(e.target.value)} />
-            </div>  
-            <div>
-                <button
-                className='px-3 py-2 cursor-pointer active:scale-95 border rounded-lg'
-                onClick={handleButtonClick}>
-                    Submit
-                </button>
-            </div>
-        </div>
         <div>
             {habits.map((habit)=>(
-            <div key={habit.id} className='flex gap-12'>
-                <div>
-                    <input 
-                    type="checkbox" 
-                    checked={habit.completed}
-                    onChange={()=> handleHabitCompletion(habit.id)}/>
-                </div>
-                <div>
-                    {habit.name}
-                </div>
-                <div>
-                    {habit.completed ? "Completed":"Not completed"}
-                </div>
-            </div>  
+                habit.name
             ))}
-        </div> */}
-
-        {JSON.stringify(habits)}
+        </div>
+        <form action={addHabit}>
+            <input type="text"
+            name="name"
+            placeholder="habit"/>
+            <button type="submit">Add</button>
+        </form>
     </div>
   )
 }
