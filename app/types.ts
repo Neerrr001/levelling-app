58ecc349a23b20ccb690072981ea7834bfef286d
Neerrr001 <neeragarwal711@gmail.com>
2026-08-30 22:11:37 +0530

    Added type Completion, Rewrote type Habit

--- app/types.ts
+++ app/types.ts
@@ -1,5 +1,11 @@
+export type Completion = {
+    id: number,
+    habitId: number, 
+    completedAt: Date
+}
 export type Habit = {
     id:number, 
     name: string, 
     userId:number,
+    completions: Completion[]
 }
