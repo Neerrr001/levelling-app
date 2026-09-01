d57c4213c88c2a7442fe1394b8c4be5773b77364
Neerrr001 <neeragarwal711@gmail.com>
2026-09-01 19:27:50 +0530

    Added comments in date.ts

--- lib/date.ts
+++ lib/date.ts
@@ -2,14 +2,14 @@ import {startOfDay, addDays } from "date-fns"
 import { fromZonedTime, toZonedTime } from "date-fns-tz"
 
 export function getTodayRange(timezone:string){
-    const now = new Date();
+    const now = new Date(); // gives us current instant
 
-    const localNow = toZonedTime(now,timezone)
-    const localStart = startOfDay(localNow);
-    const localTomorrow = addDays(localNow,1);
+    const localNow = toZonedTime(now,timezone) //convert instant to user's timezone
+    const localStart = startOfDay(localNow); // finds local midnight
+    const localTomorrow = addDays(localNow,1); // finds local tomorrow start
 
-    const todayStart = fromZonedTime(localStart, timezone);
-    const tomorrowStart = fromZonedTime(localTomorrow,timezone)
+    const todayStart = fromZonedTime(localStart, timezone); //local midnight in UTC
+    const tomorrowStart = fromZonedTime(localTomorrow,timezone) //local tomorrow in UTC
 
     return {
         todayStart, tomorrowStart
