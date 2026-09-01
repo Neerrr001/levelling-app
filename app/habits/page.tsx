6932c0ae100de7f8792c44eddd982259e91aafc3
Neerrr001 <neeragarwal711@gmail.com>
2026-09-01 20:04:46 +0530

    Added habits with completions of today only of a user

--- app/habits/page.tsx
+++ app/habits/page.tsx
@@ -1,21 +1,39 @@
 import prisma from "@/lib/prisma"
 import HabitList from "./HabitList";
+import {getTodayRange} from "@/lib/date"
+
 
 export default async function HabitsPage(){
-  const user = await prisma.user.findFirst({
+
+  const user = await prisma.user.findFirst()
+  
+  if(!user){
+    return <div>User not found</div>
+  }
+
+  const {todayStart, tomorrowStart} = getTodayRange(user?.timezone as string)
+  const habits = await prisma.habit.findMany({
+    where:{
+      userId:user.id
+    },
     include:{
-      habits:{
-        include:{
-          completions:true
+      completions:{
+        where:{
+          completedAt:{
+            gte: todayStart,
+            lt: tomorrowStart
+          }
         }
       }
     }
-  });
+  }); 
+
 
   return(
     <div>
       <h1>Hello {user?.username}</h1>
-      <HabitList habits={user?.habits ?? []}/>
+      <HabitList habits={habits}/>
+     
     </div>
   )
 }
