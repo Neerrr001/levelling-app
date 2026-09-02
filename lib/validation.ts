4b3388b14003e89a02099de2f7351cf3dff9b661
Neerrr001 <neeragarwal711@gmail.com>
2026-09-02 20:48:48 +0530

    Type bug fixed

--- lib/validation.ts
+++ lib/validation.ts
@@ -2,6 +2,6 @@ import { z } from "zod";
 
 export const ProfileSchema = z.object({
     username: z.string().min(1),
-    email: z.string(),
+    email: z.string().email(),
     timezone: z.string().min(1)
 })
