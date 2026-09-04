dc0182ba41d47b10ddbbf4a744c9fb2c95648829
Neerrr001 <neeragarwal711@gmail.com>
2026-09-04 23:13:16 +0530

    Created credentials and authorize() func

--- auth.ts
+++ auth.ts
@@ -1,12 +1,60 @@
 import NextAuth from "next-auth"
 import {authConfig} from "./auth.config"
 import Credentials from "next-auth/providers/credentials";
+import prisma from "@/lib/prisma"
+import { LoginSchema } from "@/lib/validation"
+import * as argon2 from "argon2"
+import { error } from "console";
 
 export const {auth, signIn, signOut} = NextAuth({
     ...authConfig,
     providers:[
         Credentials({
-            
+            credentials:{
+                email:{
+                    type:"email",
+                    label:"email",
+                    placeholder:"xyz@gmail.com",
+                },
+                password:{
+                    type:"password",
+                    label:"password",
+                    placeholder:"******"
+                }
+            },
+            authorize: async(credentials)=>{
+                const email = credentials.email
+                const password = credentials.password
+
+                const result = LoginSchema.safeParse({
+                    email: email,
+                    password: password,
+                })
+
+                if(!result.success){
+                    return null;
+                }
+
+                const existingUser = await prisma.user.findUnique({
+                    where:{
+                        email:result.data.email
+                    }
+                })
+
+                if(!existingUser){
+                    return null;
+                }
+
+                if(await argon2.verify(existingUser.passwordHash, result.data.password)){
+                    return{
+                        id:existingUser.id.toString(),
+                        email:existingUser.email,
+                        username:existingUser.username
+                    }
+                }
+
+            }
+
         })
     ]
 });
