3c7faaf2aa41bebb7bab28ad24373d970e0ed0ac
Neerrr001 <neeragarwal711@gmail.com>
2026-09-04 23:00:38 +0530

    Created LoginSchema using zod

--- lib/validation.ts
+++ lib/validation.ts
@@ -11,3 +11,8 @@ export const SignupSchema = z.object({
     email: z.string().email(),
     password: z.string().min(8),
 })
+
+export const LoginSchema = z.object({
+    email:z.string().email(),
+    password:z.string().min(8)
+})
